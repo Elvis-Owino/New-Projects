4971bad17e48021dd05a18413e2d053784043ccc
Elvis Owino <92543072+Elvis-Owino@users.noreply.github.com>
2022-02-17 19:52:13 +0300

    updated

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 
 gsap.registerPlugin(ScrollTrigger);
 
+//gsap.fromTo(".h", {x:"-100%",y:"0%", duration:1}, {x:"0%",y:"0%", duration:6});
+function intro(){
+	var tl=gsap.timeline();
+tl.fromTo(".a", {x:"0%",y:"-100%", duration:1}, {x:"0%",y:"0%",opacity:1, duration:6})
+.fromTo(".b", {x:"-100%",y:"0%", duration:1}, {x:"0%",y:"0%",opacity:1, duration:6})
+.fromTo(".c", {x:"0%",y:"200%", duration:1}, {x:"0%",y:"0%",opacity:1, duration:6})
+.fromTo(".d",{x:"100%",y:"0%", duration:1}, {x:"0%",y:"0%",opacity:1, duration:6})
+.fromTo(".e", {x:"0%",y:"-100%", duration:1}, {x:"0%",y:"0%",opacity:1, duration:6})
+.fromTo(".f", {x:"-100%",y:"0%", duration:1}, {x:"0%",y:"0%",opacity:1, duration:6})
+.fromTo(".g", {x:"0%",y:"200%", duration:1}, {x:"0%",y:"0%",opacity:1, duration:6})
+.fromTo(".h", {x:"100%",y:"0%", duration:1}, {x:"0%",y:"0%",opacity:1, duration:6});
+
+	
+	return tl;
+}
+
+
+
+var mastertl=gsap.timeline();
+mastertl.add(intro())
+         //.add(end(), "-=1")
+		 //.add(middle(),"+=2")
 
 
 
@@ -14,25 +36,5 @@ gsap.registerPlugin(ScrollTrigger);
 
 
 
-//gsap.to(".a", {backgroundColor:"yellow", duration:5});
-
-/* Panels. Horizontal scroll 
-
-const panelsContainer=querySelector(".panels-container")
-
-
-const panels = gsap.utils.toArray(".panel");
-gsap.to(panels, {
-	xPercent: -100 * ( panels.length - 1 ),
-	ease: "none",
-	scrollTrigger: {
-		trigger:".wrapper",
-		pin: true,
-		start: "top top",
-		scrub: 1,
-		snap: 1 / (panels.length - 1),
-		end: () =>  "+=" + (panelsContainer.offsetWidth - innerWidth)
-	}
-});*/
 
 
