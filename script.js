b60dc1d832352133b879ea171c09780c356edaaa
Elvis Owino <92543072+Elvis-Owino@users.noreply.github.com>
2022-02-18 10:52:05 +0300

    new update

--- script.js
+++ script.js
@@ -1,28 +1,50 @@
 
 gsap.registerPlugin(ScrollTrigger);
+let horizontals= gsap.utils.toArray(".horizontal");
+gsap.defaults({delay:4, toggleActions:"play pause play resume"})
 
-//gsap.fromTo(".h", {x:"-100%",y:"0%", duration:1}, {x:"0%",y:"0%", duration:6});
+
+
+
+//panels sliding in from different directions
+
 function intro(){
 	var tl=gsap.timeline();
-tl.fromTo(".a", {x:"0%",y:"-100%", duration:1}, {x:"0%",y:"0%",opacity:1, duration:6})
-.fromTo(".b", {x:"-100%",y:"0%", duration:1}, {x:"0%",y:"0%",opacity:1, duration:6})
-.fromTo(".c", {x:"0%",y:"200%", duration:1}, {x:"0%",y:"0%",opacity:1, duration:6})
-.fromTo(".d",{x:"100%",y:"0%", duration:1}, {x:"0%",y:"0%",opacity:1, duration:6})
-.fromTo(".e", {x:"0%",y:"-100%", duration:1}, {x:"0%",y:"0%",opacity:1, duration:6})
-.fromTo(".f", {x:"-100%",y:"0%", duration:1}, {x:"0%",y:"0%",opacity:1, duration:6})
-.fromTo(".g", {x:"0%",y:"200%", duration:1}, {x:"0%",y:"0%",opacity:1, duration:6})
-.fromTo(".h", {x:"100%",y:"0%", duration:1}, {x:"0%",y:"0%",opacity:1, duration:6});
+tl.fromTo(".a", {x:"0%",y:"-100%"}, {x:"0%",y:"0%",opacity:1})
+.fromTo(".b", {x:"-100%",y:"0%"}, {x:"0%",y:"0%",opacity:1})
+.fromTo(".c", {x:"0%",y:"200%"}, {x:"0%",y:"0%",opacity:1})
+.fromTo(".d",{x:"100%",y:"0%"}, {x:"0%",y:"0%",opacity:1})
+.fromTo(".e", {x:"0%",y:"0%"}, {x:"0%",y:"0%",opacity:1})
+.fromTo(".f", {x:"0%",y:"0%"}, {x:"0%",y:"0%",opacity:1})
+.fromTo(".g", {x:"0%",y:"0%"}, {x:"0%",y:"0%",opacity:1})
+.fromTo(".h", {x:"0%",y:"0%"}, {x:"0%",y:"0%",opacity:1});
+
+ScrollTrigger.create({animation:tl, trigger:".wrapper",start:"top top", scrub:"true", pin:"true", anticipatePin:1});
 
 	
 	return tl;
 }
 
+//panels moving horizontally
+function middle(){
+	var tl=gsap.timeline();
+	tl.to(horizontals, {
+		xPercent:-100 * (horizontals.length-1),
+		ease:"none",
+		scrollTrigger: {
+			trigger:".horizontals-container", pin:"true", scrub:"true", snap: 1,	
+		}
+	})
+}
+
+
 
 
 var mastertl=gsap.timeline();
-mastertl.add(intro())
-         //.add(end(), "-=1")
-		 //.add(middle(),"+=2")
+mastertl .add(intro())
+        .add(middle())
+       
+     
 
 
 
